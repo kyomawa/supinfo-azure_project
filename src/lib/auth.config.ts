454f5cb9e1d7c0b736876676b7d428135101fcf7
kyomawa <kyomawa@protonmail.com>
2024-11-15 09:34:40 +0100

    feat: same email on multiple providers now working

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -3,7 +3,14 @@ import Google from "next-auth/providers/google";
 import Facebook from "next-auth/providers/facebook";
 
 const authConfig: NextAuthConfig = {
-  providers: [Google, Facebook],
+  providers: [
+    Google({
+      allowDangerousEmailAccountLinking: true,
+    }),
+    Facebook({
+      allowDangerousEmailAccountLinking: true,
+    }),
+  ],
   trustHost: true,
 };
 
