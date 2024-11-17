651b23e75d128f70ed0e026d87114cc05b83b807
kyomawa <kyomawa@protonmail.com>
2024-11-17 19:10:10 +0100

    feat: working on parameters page

--- src/app/(WebApp)/parametres/page.tsx
+++ src/app/(WebApp)/parametres/page.tsx
@@ -1,11 +1,13 @@
 import { settingsMetadata } from "@/constants/metadata";
+import SettingsPage from "./components/SettingsPage";
 
 export const metadata = settingsMetadata;
 
 export default function Page() {
   return (
-    <div className="p-6">
+    <div className="p-6 flex flex-col gap-y-6">
       <h1 className="text-4xl font-medium">Paramètres</h1>
+      <SettingsPage />
     </div>
   );
 }
